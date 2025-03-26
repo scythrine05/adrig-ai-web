e5f450870159ba5b489801b0d2a1a1f9fc71603b
Rohan Murmu <scythrine05@gmail.com>
2025-03-26 17:47:02 +0530

    Added Animation Components

---
+++ src/animations/card-animation/Workflow_Automations.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Content_creation() {
+  return (
+     <Image
+         src="/animation-assets/workflow-auto.png"
+         alt="Workflow-Automations"
+         width={600}
+         height={400}
+         layout="responsive"
+       />
+  )
+}
--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,4 +1,4 @@
-export const ServiceCard = ({ title, description, size = "small" }) => {
+export const ServiceCard = ({ title, description, size = "small", animation }) => {
     return (
       <div
         className={`rounded-xl shadow-md p-6 flex flex-col items-center justify-between 
@@ -8,8 +8,8 @@ export const ServiceCard = ({ title, description, size = "small" }) => {
               [background-image:linear-gradient(white,white),linear-gradient(150deg,rgba(17,85,214,0.8),rgba(255,255,255,0.6))] 
               [background-origin:border-box] [background-clip:padding-box,border-box]`}
       >
-        <div className="w-full h-40 bg-gray-400 rounded-lg mb-4" />
-        <div className="w-full flex flex-col flex-grow">
+        {animation}
+        <div className="w-full flex flex-col flex-grow mt-5">
           <h2 className="text-xl font-semibold">{title}</h2>
           <div className="mt-4 mb-10 flex-grow">
             <p className="text-sm">{description}</p>
